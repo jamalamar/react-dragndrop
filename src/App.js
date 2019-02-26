527dac251031c3c55bfabd24533ad53c4d37c42b
jamalamar <jamal_ab22@hotmail.com>
2019-02-26 09:38:06 -0600

    working drag and drop list

--- src/App.js
+++ src/App.js
@@ -5,9 +5,37 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
+state = {
     items: ["🍰 Cake", "🍩 Donut", "🍎 Apple", "🍕 Pizza"]
-  }
+  };
+
+  onDragStart = (e, index) => {
+    this.draggedItem = this.state.items[index];
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+    e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
+  };
+
+  onDragOver = index => {
+    const draggedOverItem = this.state.items[index];
+
+    // if the item is dragged over itself, ignore
+    if (this.draggedItem === draggedOverItem) {
+      return;
+    }
+
+    // filter out the currently dragged item
+    let items = this.state.items.filter(item => item !== this.draggedItem);
+
+    // add the dragged item after the dragged over item
+    items.splice(index, 0, this.draggedItem);
+
+    this.setState({ items });
+  };
+
+  onDragEnd = () => {
+    this.draggedIdx = null;
+  };
 
   render() {
     return (
@@ -15,12 +43,17 @@ class App extends Component {
         <main>
           <h3>List of items</h3>
           <ul>
-            {this.state.items.map(item => (
-              <li key={item}>
-                <div className="drag">
-                  <Hamburger />
+            {this.state.items.map((item, idx) => (
+              <li key={item} onDragOver={() => this.onDragOver(idx)}>
+                <div
+                  className="drag"
+                  draggable
+                  onDragStart={e => this.onDragStart(e, idx)}
+                  onDragEnd={this.onDragEnd}
+                >
+                  <p>⌸</p>
                 </div>
-                {item}
+                <span className="content">{item}</span>
               </li>
             ))}
           </ul>
